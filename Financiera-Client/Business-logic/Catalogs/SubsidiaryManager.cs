f1bb51d552ec13fd23ece0f376f3a79bbdf88fd8
guashasha <davidcarrionr00@hotmail.com>
2025-05-06 17:36:53 -0600

    feat: finished subsidiary management
    added table rows for subsidiary management, connected window with admin main menu

--- Financiera-Client/Business-logic/Catalogs/SubsidiaryManager.cs
+++ Financiera-Client/Business-logic/Catalogs/SubsidiaryManager.cs
@@ -3,18 +3,96 @@ using DomainClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Business_logic.Catalogs
 {
-    internal class SubsidiaryManager
+    public class SubsidiaryManager
     {
         public List<Subsidiary> GetAll()
         {
             CatalogServiceClient client = new();
+            ResponseWithContentOfArrayOfSubsidiaryDC1nk_PiFui response;
 
-            return client.getSubsidiariesList();
+            try
+            {
+                response = client.GetSubsidiaries();
+            }
+            catch (CommunicationException error)
+            {
+                // log error
+                throw new Exception(ErrorMessages.ServerError);
+            }
+
+            List<Subsidiary> subsidiaries = new List<Subsidiary>();
+
+            foreach (SubsidiaryDC subsidiary in response.Data) 
+            {
+                subsidiaries.Add(new Subsidiary()
+                {
+                    Id = subsidiary.Id,
+                    Address = subsidiary.Address,
+                    State = subsidiary.State
+                });
+            }
+
+            return subsidiaries;
+        }
+
+        public int Add(string address)
+        {
+            CatalogServiceClient client = new();
+            Response response;
+
+            try
+            {
+                response = client.AddSubsidiary(address);
+            }
+            catch (CommunicationException error)
+            {
+                // log response.Message
+                throw new Exception(ErrorMessages.ServerError);
+            }
+
+            return response.StatusCode;
+        }
+
+        public int updateAddress(int id, string address)
+        {
+            CatalogServiceClient client = new();
+            Response response;
+
+            try
+            {
+                response = client.UpdateSubsidiaryAddress(id, address);
+            }
+            catch (CommunicationException error)
+            {
+                // log response.Message
+                throw new Exception(ErrorMessages.ServerError);
+            }
+
+            return response.StatusCode;
+        }
+
+        public int updateState(int id, bool state)
+        {
+            CatalogServiceClient client = new();
+            Response response;
+
+            try
+            {
+                response = client.UpdateSubsidiaryState(id, state);
+            }
+            catch (CommunicationException error)
+            {
+                // log response.Message
+                throw new Exception(ErrorMessages.ServerError);
+            }
+
+            return response.StatusCode;
         }
     }
 }
--- Financiera-Client/Financiera-GUI/CatalogManagement/wFinancialBranchManagement.xaml.cs
+++ Financiera-Client/Financiera-GUI/CatalogManagement/wFinancialBranchManagement.xaml.cs
@@ -1,4 +1,6 @@
-﻿using DomainClasses;
+﻿using Business_logic.Catalogs;
+using DomainClasses;
+using Financiera_GUI.Utilities;
 using Notification.Wpf;
 using System;
 using System.Collections.Generic;
@@ -20,11 +22,100 @@ namespace Financiera_GUI.CatalogManagement
 {
     public partial class wFinancialBranchManagement : Page
     {
-        private List<Subsidiary> subsidiaries;
+        private List<Subsidiary> _subsidiaries;
+        private NotificationManager _notificationManager = new();
+
+        private int _currentPage = 1;
+        private static int PAGE_SIZE = 12;
+
         public wFinancialBranchManagement()
         {
             InitializeComponent();
-            subsidiaries = Catalog
+            SubsidiaryManager subsidiaryManager = new();
+
+            try
+            {
+                _subsidiaries = subsidiaryManager.GetAll();
+            }
+            catch (Exception error)
+            {
+                var notificationManager = new NotificationManager();
+                notificationManager.Show("Error al registrar", "Ocurrió un error de nuestro lado, intente nuevamente", NotificationType.Error, "WindowArea");
+                _subsidiaries = [];
+                return;
+            }
+
+            UpdateList();
+        }
+
+        private void UpdateList()
+        {
+            subsidiariesTable.Children.RemoveRange(1, subsidiariesTable.Children.Count);
+            for (int i = (_currentPage * PAGE_SIZE) - PAGE_SIZE; i < int.Min(_subsidiaries.Count, _currentPage * PAGE_SIZE); i++)
+            {
+                subsidiariesTable.Children.Add(new ucSubsidiaryManagementRow(_subsidiaries[i]));
+            }
+
+            if (_currentPage * PAGE_SIZE < _subsidiaries.Count)
+            {
+                nextPageButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                nextPageButton.Visibility = Visibility.Visible;
+            }
+
+            if (_currentPage == 1)
+            {
+                previousPageButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                previousPageButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Register(object sender, MouseButtonEventArgs e)
+        {
+            string address = addressInput.Text.Trim();
+            if (string.IsNullOrEmpty(address))
+            {
+                _notificationManager.Show("Error", "No se puede agregar una sucursal sin dirección", NotificationType.Warning, "WindowArea");
+                return;
+            }
+
+            SubsidiaryManager subsidiaryManager = new();
+            switch (subsidiaryManager.Add(address))
+            {
+                case 0:
+                    UpdateList();
+                    _notificationManager.Show("Registrado", "La sucursal se registró correctamente", NotificationType.Success, "WindowArea");
+                    break;
+                
+                case 1:
+                    _notificationManager.Show("Error al registrar", "Ocurrió un error de nuestro lado, intente nuevamente", NotificationType.Error, "WindowArea");
+                    break;
+
+                case 2:
+                    _notificationManager.Show("Error al registrar", "La dirección ingresada está vacía o contiene caracteres no validos", NotificationType.Warning, "WindowArea");
+                    break;
+
+                case 3:
+                    _notificationManager.Show("Error al registrar", "La sucursal que intentó registrar ya existe", NotificationType.Warning, "WindowArea");
+                    break;
+            }
+        }
+
+        private void PreviousPage(object sender, MouseButtonEventArgs e)
+        {
+            pageCounter.Content = (int)pageCounter.Content - 1;
+            UpdateList();
+        }
+
+        private void NextPage(object sender, MouseButtonEventArgs e)
+        {
+            pageCounter.Content = (int)pageCounter.Content + 1;
+            UpdateList();
         }
     }
 }
