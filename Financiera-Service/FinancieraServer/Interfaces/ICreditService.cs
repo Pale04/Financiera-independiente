0739c8c8f281d35c9189113cac2c990a95dd032d
guashasha <davidcarrionr00@hotmail.com>
2025-05-17 19:06:17 -0600

    feat: implemented getting credit requests on backend and created credit requests view on database scripts

--- Financiera-Service/FinancieraServer/Interfaces/ICreditService.cs
+++ Financiera-Service/FinancieraServer/Interfaces/ICreditService.cs
@@ -15,7 +15,7 @@ namespace FinancieraServer.Interfaces
         ResponseWithContent<List<CreditDC>> GetCreditsByBeneficiary(int beneficiaryId);
 
         [OperationContract]
-        ResponseWithContent<List<CreditRequestDC>> GetCreditRequests();
+        ResponseWithContent<List<CreditRequestSummaryDC>> GetCreditRequests();
 
         [OperationContract]
         Response DetermineRequest(int requestId, bool granted);
