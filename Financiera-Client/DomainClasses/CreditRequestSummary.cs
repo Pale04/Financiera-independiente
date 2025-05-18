b2a835812a1033287965bf8d4578feee04c80127
guashasha <davidcarrionr00@hotmail.com>
2025-05-17 22:43:58 -0600

    feat: implemented credit requests list on frontend
    made backend get only 100 requests, created credit requests window and transition to credit info

---
+++ Financiera-Client/DomainClasses/CreditRequestSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace DomainClasses
+{
+    public class CreditRequestSummary
+    {
+        public int Id;
+        public int Duration;
+        public int Capital;
+        public string ClientName;
+        public int InterestRate;
+    }
+}
