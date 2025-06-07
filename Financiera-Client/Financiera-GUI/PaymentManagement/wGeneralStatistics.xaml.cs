e4aad1dbb865acfb125459b1daf978d139358432
guashasha <davidcarrionr00@hotmail.com>
2025-06-06 18:18:40 -0600

    feat: implemented statistics window
    removed manage uncollectable credits button, and finished general statistics about credit payments

--- Financiera-Client/Financiera-GUI/PaymentManagement/wGeneralStatistics.xaml.cs
+++ Financiera-Client/Financiera-GUI/PaymentManagement/wGeneralStatistics.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Business_logic.Payments;
+using DomainClasses;
+using Financiera_GUI.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,14 +20,69 @@ namespace Financiera_GUI.PaymentManagement
 {
     public partial class wGeneralStatistics : Page
     {
+        private List<Payment>? _payments;
+        private DateTime _firstDate = DateTime.Now.AddMonths(-1);
+        private DateTime _lastDate = DateTime.Now;
+
         public wGeneralStatistics()
         {
             InitializeComponent();
+            _payments = GetPayments();
+            if (_payments != null)
+            {
+                RefreshRows();
+                SetStatistics();
+            }
+        }
+        private void SetStatistics()
+        {
+            double total = 0;
+            double totalCollected = 0;
+            double collectedPayments = 0;
+
+            foreach (Payment payment in _payments)
+            {
+                total += (double)payment.Amount;
+
+                if (payment.GetState().Equals(PaymentStatus.Collected))
+                {
+                    totalCollected += (double)payment.Amount;
+                    collectedPayments++;
+                }
+            }
+
+            totalCreditsLabel.Content = _payments.Count.ToString();
+            totalAmountLabel.Content = total;
+            totalCollectedLabel.Content = totalCollected;
+            collectionEficiencyLabel.Content = $"{(collectedPayments/ _payments.Count)*100}%";
+        }
+
+        private void RefreshRows()
+        {
+            paymentsPanel.Children.Clear();
+
+            foreach (Payment payment in _payments)
+            {
+                paymentsPanel.Children.Add(new ucStatisticsRow()
+                {
+                    CreditId = payment.CreditId.ToString(),
+                    PaymentDate = payment.CollectionDate.ToString(),
+                    State = payment.GetState().ToString(),
+                    Amount = payment.Amount.ToString()
+                });
+            }
+        }
+
+        private List<Payment>? GetPayments()
+        {
+            PaymentManager manager = new();
+
+            return manager.GetPaymentsFromDateRange(_firstDate, _lastDate);
         }
 
         private void Back(object sender, MouseButtonEventArgs e)
         {
-
+            NavigationService.GoBack();
         }
     }
 }
--- Financiera-Client/Financiera-GUI/Utilities/ucStatisticsRow.xaml.cs
+++ Financiera-Client/Financiera-GUI/Utilities/ucStatisticsRow.xaml.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace Financiera_GUI.Utilities
 {
-    /// <summary>
-    /// Lógica de interacción para ucStatisticsRow.xaml
-    /// </summary>
     public partial class ucStatisticsRow : UserControl
     {
+        public string CreditId
+        {
+            set => creditLabel.Content = value;
+        }
+
+        public string PaymentDate
+        {
+            set => dateLabel.Content = value;
+        }
+
+        public string Amount
+        {
+            set => amountLabel.Content = value;
+        }
+
+        public string State
+        {
+            set => stateLabel.Content = value;
+        }
+
         public ucStatisticsRow()
         {
             InitializeComponent();
